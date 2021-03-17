e65e819e48028d0c549e36a27e4242276aaad7a3
Vladislavs Packo <v.packo94@gmail.com>
2021-03-17 13:32:10 +0100

    feat: WIP segment display

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,87 @@
 #include <Arduino.h>
 #include <avr/pgmspace.h>
-#include "../lib/MusicPlayer/src/MusicPlayer.h"
 
-MusicPlayer<64, 5> *mp;
+const uint8_t PINS[8] PROGMEM = {2, 3, 4, 5, 6, 8, 7, 9};
 
-/**
- * Bare minimum notes for a simple track
- *
- * A4 = 440 Hz
- * @see https://www.szynalski.com/tone-generator/
- */
-const uint16_t NOTES[5] PROGMEM = {
-        0,   //Rest 0
-        494, //B4   1
-        622, //D#5  2
-        659, //E5   3
-        740, //F#5  4
+const uint8_t MAP[19] PROGMEM = {
+        //PGFEDCBA
+        0b00111111, // 0
+        0b00000110, // 1
+        0b01011011, // 2
+        0b01001111, // 3
+        0b01100110, // 4
+        0b01101101, // 5
+        0b01111101, // 6
+        0b00000111, // 7
+        0b01111111, // 8
+        0b01101111, // 9
+        0b00000000, // nothing
+        0b00000001, // seg a
+        0b00000010, // seg b
+        0b00000100, // seg c
+        0b00001000, // seg d
+        0b00010000, // seg e
+        0b00100000, // seg f
+        0b01000000, // seg g
+        0b10000000, // seg dp
 };
 
-/**
- * {NOTE, TICKS}
- * There are 192 ticks in one bar
- * (96 ticks in one second with 120 bpm)
- *
- * Street Fighter - Guile's Theme
- * @see https://www.songsterr.com/a/wsa/super-street-fighter-ii-guiles-theme-power-trio-tab-s457584t3
- */
-//@formatter:off
-const uint8_t TRACK[64][2] PROGMEM = {
-        {3, 12}, {0, 12}, {3, 12}, {2, 12},  {0, 12}, {2, 12},  {3, 96},  {2, 12}, {0, 12},
-        {3, 12}, {0, 12}, {3, 12}, {2, 12},  {0, 12}, {2, 12},  {3, 96},  {2, 12}, {0, 12},
-        {3, 12}, {2, 12}, {0, 12}, {3, 12},  {0, 12}, {2, 12}, {0, 12}, {4, 12},  {0, 12}, {4, 12}, {3, 24},  {2, 24}, {1, 24}
-};
-//@formatter:on
+class SegmentDisplay {
+public:
+//    struct RENDER {
+//        uint8_t ZERO = 0;
+//        uint8_t ONE = 1;
+//        uint8_t TWO = 2;
+//        uint8_t THREE = 3;
+//        uint8_t FOUR = 4;
+//        uint8_t FIVE = 5;
+//        uint8_t SIX = 6;
+//        uint8_t SEVEN = 7;
+//        uint8_t EIGHT = 8;
+//        uint8_t NINE = 9;
+//        uint8_t NOTHING = 10;
+//        uint8_t SEG_A = 11;
+//        uint8_t SEG_B = 12;
+//        uint8_t SEG_C = 13;
+//        uint8_t SEG_D = 14;
+//        uint8_t SEG_E = 15;
+//        uint8_t SEG_F = 16;
+//        uint8_t SEG_G = 17;
+//        uint8_t SEG_DP = 18;
+//    };
 
-// Example storing and reading data from flash memory.
-const uint8_t TRACK_PROPS[2] PROGMEM = {120, 31};
+    explicit SegmentDisplay(const uint8_t (*pins)[8], const uint8_t (*map)[19]) : _pins(pins), _map(map) {
+        for (uint8_t i = 0; i < 8; i++) {
+            const uint8_t p = pgm_read_byte(&_pins[0][i]);
+            if (p) pinMode(p, OUTPUT);
+        }
+    }
+
+    void render(const uint8_t symbol) {
+        const uint8_t s = pgm_read_byte(&_map[0][symbol]);
+        for (uint8_t i = 0; i < 8; i++) {
+            const uint8_t p = pgm_read_byte(&_pins[0][i]);
+            if (p) digitalWrite(p, (s & (1 << i)) != 0);
+        }
+    }
+
+    void clear() {
+        render(10);
+    }
+
+private:
+    const uint8_t (*_pins)[8];
+
+    const uint8_t (*_map)[19];
+};
+
+SegmentDisplay *sd;
 
 void setup() {
-    Serial.begin(9600);
-
-    mp = new MusicPlayer<64, 5>(11);
-
-    mp->setNotes(&NOTES);
-    mp->setTrack(&TRACK);
-    mp->setupTrack(
-            (uint8_t) pgm_read_byte(&TRACK_PROPS[0]),
-            (uint16_t) pgm_read_byte(&TRACK_PROPS[1])
-    );
-
-    // End of track basic loop example
-    // Can be used to implement own logic
-    // (next track for example etc.)
-    mp->setOnEnd([] { mp->reset(); });
-
-    mp->play();
+    sd = new SegmentDisplay(&PINS, &MAP);
+    sd->render(9);
 }
 
 void loop() {
-    unsigned long int micro = micros();
 
-    mp->loop(micro);
 }
